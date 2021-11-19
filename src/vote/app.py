72c4177ae9cea2ad6dd52a96c88f351c9507fcb3
Tony Pujals <tonypujals@google.com>
2021-11-18 21:51:38 -0800

    wip: frontend

--- src/vote/app.py
+++ src/vote/app.py
@@ -1,51 +1,58 @@
-from flask import Flask, render_template, request, make_response, g
-from redis import Redis
-import os
-import socket
-import random
 import json
 import logging
+import os
+import random
+import requests
+import socket
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
-hostname = socket.gethostname()
+from flask import Flask, render_template, request, make_response
+from paste.translogger import TransLogger
+from waitress import serve
 
 app = Flask(__name__)
-
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger = logging.getLogger("app")
 app.logger.setLevel(logging.INFO)
 
-def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
-    return g.redis
+# data for rendering UI
+option_a = os.getenv("OPTION_A", "Tabs")
+option_b = os.getenv("OPTION_B", "Spaces")
 
-@app.route("/", methods=['POST','GET'])
-def hello():
-    voter_id = request.cookies.get('voter_id')
+host = os.getenv("HOST", "0.0.0.0")
+port = os.getenv("PORT", "8080")
+hostname = socket.gethostname()
+api = os.getenv("API", "http://api:8080")
+
+
+@app.route("/", methods=["GET", "POST"])
+def handle_vote():
+    voter_id = request.cookies.get("voter_id")
     if not voter_id:
         voter_id = hex(random.getrandbits(64))[2:-1]
 
     vote = None
 
-    if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        app.logger.info('Received vote for %s', vote)
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
+    if request.method == "POST":
+        vote = request.form["vote"]
+        data = {"voter_id": voter_id, "vote": vote}
+        app.logger.info(data)
+        r = requests.post(api + "/vote", json=data)
+
+        app.logger.info(f"status code: {r.status_code}")
+        print(r, flush=True)
+        print('----------')
 
     resp = make_response(render_template(
-        'index.html',
+        "index.html",
         option_a=option_a,
         option_b=option_b,
         hostname=hostname,
         vote=vote,
     ))
-    resp.set_cookie('voter_id', voter_id)
+    resp.set_cookie("voter_id", voter_id)
     return resp
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    display_host = "" if host == "0.0.0.0" else host
+    print(f"serving on {display_host}:{port}", flush=True)
+    serve(TransLogger(app), host=host, port=port)
